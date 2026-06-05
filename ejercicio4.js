4e2fb9be99cde8289ddca1a54b6f93adf09aa6c9
Alfonso <alfonsoguzmanguerrero@gmail.com>
2026-06-05 11:15:43 +0200

    Primer commint ejercicio-repaso4

---
+++ ejercicio4.js
@@ -0,0 +1,14 @@
+// 1. Mostrar un mensaje en la consola del navegador
+console.log("¡Hola! El archivo JavaScript se ha cargado correctamente.");
+
+// 2. Crear variables (guardar datos)
+const nombre = "Carlos";
+let edad = 25;
+
+// 3. Crear una función (un bloque de código reutilizable)
+function saludar(usuario) {
+    console.log("Hola, " + usuario + ". ¡Bienvenido a la programación!");
+}
+
+// 4. Ejecutar (llamar) la función
+saludar(nombre);
